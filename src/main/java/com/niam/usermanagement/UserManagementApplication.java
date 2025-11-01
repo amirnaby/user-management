fa39f9de7f928e365ed19ba5af3cb43c60b7c1d8
r.naby <r.naby@behsacorp.com>
2025-11-01 15:25:37 +0330

    Initial Commit

--- src/main/java/com/niam/usermanagement/UserManagementApplication.java
+++ src/main/java/com/niam/usermanagement/UserManagementApplication.java
@@ -3,7 +3,7 @@ package com.niam.usermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.niam")
 public class UserManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserManagementApplication.class, args);
