edd4cb25d20ce1d58a94d843903989132ad00c4b
r.naby <r.naby@behsacorp.com>
2025-11-17 13:08:45 +0330

    Initial Commit

--- src/main/java/com/niam/usermanagement/service/LockService.java
+++
@@ -1,8 +0,0 @@
-package com.niam.usermanagement.service;
-
-import com.niam.usermanagement.model.entities.User;
-
-public interface LockService {
-    void lockUser(User user, long lockSeconds);
-    void unlockIfExpired(User user);
-}
