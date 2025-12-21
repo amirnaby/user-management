ae8f812b69c0d7e38fb15626f48891c1d632cab2
r.naby <r.naby@behsacorp.com>
2025-12-21 09:02:34 +0330

    minor changes

--- src/main/java/com/niam/usermanagement/config/aop/PermissionAspect.java
+++ src/main/java/com/niam/usermanagement/config/aop/PermissionAspect.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -22,9 +23,11 @@ public class PermissionAspect {
 
         String[] required = Arrays.stream(hasPermission.value()).map(Object::toString).toArray(String[]::new);
 
-        boolean allowed = userPerms.stream()
-                .map(GrantedAuthority::getAuthority)
-                .anyMatch(Arrays.asList(required)::contains);
+        boolean allowed;
+        if (userPerms.contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) allowed = true;
+        else allowed = userPerms.stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .anyMatch(Arrays.asList(required)::contains);
 
         if (!allowed) {
             throw new AccessDeniedException("Permission denied. Required: " + Arrays.toString(required));
